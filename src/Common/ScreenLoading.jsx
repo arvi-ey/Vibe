3d6697dd1e55d06c338e792a08361d8e6db8985e
arvi-ey <tuhinroyand@gmail.com>
2025-08-18 12:31:15 +0530

    Loading changed with logo

--- src/Common/ScreenLoading.jsx
+++ src/Common/ScreenLoading.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import Loader from "../../src/assets/Animation/loader.json"
 import Lottie from 'lottie-react';
+import Logo from "../assets/logo.svg"
 
 const ScreenLoading = () => {
     return (
         <div className='w-screen h-screen flex justify-center items-center' >
-            <Lottie
-                animationData={Loader}
-                loop
-                autoplay
-            />
+            <img src={Logo} className='size-50' />
         </div>
     )
 }
